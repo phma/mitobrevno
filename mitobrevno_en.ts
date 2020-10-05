55a582f327b4e47ab0a4f4aa87bae2272178cd55
Pierre Abbat <phma@bezitopo.org>
2020-10-05 06:57:53 +0000

    Add translation files

---
+++ mitobrevno_en.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="54"/>
+        <source>Open Log File</source>
+        <translation>Open log file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="58"/>
+        <source>(*.log);;(*)</source>
+        <translation>Log files (*.log);;All files (*)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="71"/>
+        <location filename="mainwindow.cpp" line="72"/>
+        <location filename="mainwindow.cpp" line="79"/>
+        <source>Mitobrevno</source>
+        <translation>Mitobrevno</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="73"/>
+        <source>%1
+Version %2
+Copyright %3 Pierre Abbat
+License Apache 2</source>
+        <translation>%1
+Version %2
+Copyright %3 Pierre Abbat
+License Apache 2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>&amp;File</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="86"/>
+        <source>&amp;View</source>
+        <translation>&amp;View</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="87"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="92"/>
+        <source>Open</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="97"/>
+        <source>Exit</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>About Mitobrevno</source>
+        <translation>About Mitobrevno</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="106"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="mainwindow.cpp" line="28"/>
+        <source>Mitobrevno</source>
+        <translation>Mitobrevno</translation>
+    </message>
+</context>
+</TS>
---
+++ mitobrevno_es.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="54"/>
+        <source>Open Log File</source>
+        <translation>Abrir fichero historial</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="58"/>
+        <source>(*.log);;(*)</source>
+        <translation>Ficheros historiales (*.log);;Todos los ficheros (*)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="71"/>
+        <location filename="mainwindow.cpp" line="72"/>
+        <location filename="mainwindow.cpp" line="79"/>
+        <source>Mitobrevno</source>
+        <translation>Mitobrevno</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="73"/>
+        <source>%1
+Version %2
+Copyright %3 Pierre Abbat
+License Apache 2</source>
+        <translation>%1
+Versión %2
+Copyright %3 Pierre Abbat
+Licencia Apache 2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichero</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="86"/>
+        <source>&amp;View</source>
+        <translation>&amp;Ver</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="87"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Ajustes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Ayuda</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="92"/>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="97"/>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>About Mitobrevno</source>
+        <translation>Acerca de Mitobrevno</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="106"/>
+        <source>About Qt</source>
+        <translation>Acerca de Qt</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="mainwindow.cpp" line="28"/>
+        <source>Mitobrevno</source>
+        <translation>Mitobrevno</translation>
+    </message>
+</context>
+</TS>
